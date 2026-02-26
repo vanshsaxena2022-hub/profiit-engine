53d2b57ca38e7529098b7f7776b007f4e001b9b3
Vansh <vanshsaxena2022@gmail.com>
2026-02-26 11:18:20 +0530

    admin login fix

--- src/app/api/admin-login/route.ts
+++ src/app/api/admin-login/route.ts
@@ -2,8 +2,10 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const body = await req.json();
-
   const { email, password } = body;
+
+  console.log("ENV EMAIL:", process.env.SUPER_ADMIN_EMAIL);
+  console.log("ENV PASSWORD:", process.env.SUPER_ADMIN_PASSWORD);
 
   if (
     email === process.env.SUPER_ADMIN_EMAIL &&
