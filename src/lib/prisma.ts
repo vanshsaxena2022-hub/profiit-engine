7547afe0194635e3a70d4e300fadda884ff36c69
Vansh <vanshsaxena2022@gmail.com>
2026-03-02 00:18:31 +0530

    AR fixed 4.1

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
+  prisma: PrismaClient
+}
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error"],
-  });
+  globalForPrisma.prisma ||
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+  globalForPrisma.prisma = prisma
 }
