97d73704dea1036720332e530aec33171365455e
Vansh <vanshsaxena2022@gmail.com>
2026-03-03 18:02:02 +0530

    DB FIX 8

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -4,26 +4,16 @@ export const revalidate = 0
 
 import { prisma } from "@/lib/prisma"
 
-export default async function ProductPage() {
+export default async function ProductPage({
+  params,
+}: {
+  params: { id: string }
+}) {
 
-  try {
-    const result: any = await prisma.$queryRawUnsafe(`
-      SELECT current_database(), current_schema();
-    `)
-
-    return (
-      <div style={{ padding: 40 }}>
-        <h1>DB Info</h1>
-        <pre>{JSON.stringify(result, null, 2)}</pre>
-      </div>
-    )
-
-  } catch (error: any) {
-    return (
-      <div style={{ padding: 40 }}>
-        <h1>DB Error</h1>
-        <pre>{error?.message}</pre>
-      </div>
-    )
-  }
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Param Debug</h1>
+      <pre>{JSON.stringify(params, null, 2)}</pre>
+    </div>
+  )
 }
