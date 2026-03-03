d110a710c3ffa37b324c0781ee915128de078f33
Vansh <vanshsaxena2022@gmail.com>
2026-03-03 18:26:34 +0530

    DB FIX 10

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,17 +1,18 @@
 export const dynamic = "force-dynamic"
 export const runtime = "nodejs"
 export const revalidate = 0
-export const dynamicParams = true
 
 export default async function ProductPage({
   params,
 }: {
-  params: { id: string }
+  params: Promise<{ id: string }>
 }) {
+  const resolvedParams = await params
+
   return (
     <div style={{ padding: 40 }}>
       <h1>Param Debug</h1>
-      <pre>{JSON.stringify(params, null, 2)}</pre>
+      <pre>{JSON.stringify(resolvedParams, null, 2)}</pre>
     </div>
   )
 }
